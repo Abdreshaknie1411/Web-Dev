07c6f79cff2a3346c5482ba4cffc8ca5b58aa3bc
пуауа <your_email@whatever.com>
2025-02-13 14:49:47 +0600

    ChangeLab3

--- Lab3/ToDo List/main.js
+++ Lab3/ToDo List/main.js
@@ -2,16 +2,50 @@ function addTask() {
     let taskInput = document.getElementById("taskInput");
     let taskText = taskInput.value.trim();
     if (taskText === "") return;
+    taskText=taskText.charAt(0).toUpperCase()+taskText.slice(1).toLowerCase();
+
+    if(taskText.length===1){
+        return;
+        //alert("could not be added to the list");
+    }
+    else if(taskText.length===1){
+        alert("Could not be added to the list")
+    }
 
     let li = document.createElement("li");
-    li.innerHTML = `${taskText} <button onclick="removeTask(this)">❌</button>`;
-    li.addEventListener("click", function() {
-        li.classList.toggle("completed");
-    });
+
+    let checkBox = document.createElement("input")
+    checkBox.type="checkbox";
+    checkBox.addEventListener("change", function(){
+        if(this.checked){
+            span.style.textDecoration = "line-through";
+        }
+        else {
+            span.style.textDecoration = "none";
+        }
+    })
+   
+    let span = document.createElement("span");
+    span.textContent=taskText;
+
+
+    li.appendChild(checkBox);
+    li.appendChild(span);
+
     document.getElementById("taskList").appendChild(li);
-    taskInput.value = "";
+    taskInput.value="";
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.addEventListener('click',function(){
+        let test = confirm("Remove?");
+        if(test){
+            li.remove();
+        }
+        
+    })  
+    li.appendChild(deleteButton);
+    
+   
 }
 
-function removeTask(button) {
-    button.parentElement.remove();
-}
