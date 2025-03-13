510c1d624c080f93aafebcf228846723f5f887cd
пуауа <your_email@whatever.com>
2025-03-13 12:58:14 +0500

    Bonus

--- BONUS_TASK/Task/src/app/task-item/task-item.component.ts
+++ BONUS_TASK/Task/src/app/task-item/task-item.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BaseTaskComponent } from '../base-task/base-task.component';
+import { Router } from '@angular/router'; 
 
 @Component({
+  standalone:true,
   selector: 'app-task-item',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
-export class TaskItemComponent {
+export class TaskItemComponent extends BaseTaskComponent{
+
+  @Output() completed = new EventEmitter<number>();
+
+  constructor(private router: Router) { super(); }
+
+  markAsCompleted(event: Event) {
+    event.stopPropagation(); 
+    this.completed.emit(this.task.id);
+  }
+
+  viewDetails() {
+    this.router.navigate(['/tasks', this.task.id]);
+  }
+
+
 
 }
